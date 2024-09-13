3cb06094cada41d405bfa6f9f2d2a10a02aca7fb
Aldi Dharmawan <aldidharmawan700@gmail.com>
2024-09-13 16:42:11 +0700

    yes

--- resources/views/components/headers/dd-apps-mobile.blade.php
+++ resources/views/components/headers/dd-apps-mobile.blade.php
@@ -1,171 +1,132 @@
 <!--  Mobilenavbar -->
 <div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="mobilenavbar"
-  aria-labelledby="offcanvasWithBothOptionsLabel">
-  <nav class="sidebar-nav scroll-sidebar">
-    <div class="offcanvas-header justify-content-between">
-      <img src="{{ URL::asset('build/images/logos/favicon.ico') }}" alt="modernize-img" class="img-fluid" />
-      <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-    </div>
-    <div class="offcanvas-body h-n80" data-simplebar="" data-simplebar>
-      <ul id="sidebarnav">
-        <li class="sidebar-item">
-          <a class="sidebar-link has-arrow" href="javascript:void(0)" aria-expanded="false">
-            <span>
-              <i class="ti ti-apps"></i>
-            </span>
-            <span class="hide-menu">Apps</span>
-          </a>
-          <ul aria-expanded="false" class="collapse first-level my-3">
-            <li class="sidebar-item py-2">
-              <a href="/main/app-chat" class="d-flex align-items-center">
-                <div class="text-bg-light rounded-1 me-3 p-6 d-flex align-items-center justify-content-center">
-                  <img src="{{ URL::asset('build/images/svgs/icon-dd-chat.svg') }}" alt="modernize-img" class="img-fluid"
-                    width="24" height="24" />
-                </div>
-                <div>
-                  <h6 class="mb-1 bg-hover-primary">Chat Application</h6>
-                  <span class="fs-2 d-block text-muted">New messages arrived</span>
-                </div>
-              </a>
-            </li>
-            <li class="sidebar-item py-2">
-              <a href="/main/app-invoice" class="d-flex align-items-center">
-                <div class="text-bg-light rounded-1 me-3 p-6 d-flex align-items-center justify-content-center">
-                  <img src="{{ URL::asset('build/images/svgs/icon-dd-invoice.svg') }}" alt="modernize-img" class="img-fluid"
-                    width="24" height="24" />
-                </div>
-                <div>
-                  <h6 class="mb-1 bg-hover-primary">Invoice App</h6>
-                  <span class="fs-2 d-block text-muted">Get latest invoice</span>
-                </div>
-              </a>
-            </li>
-            <li class="sidebar-item py-2">
-              <a href="/main/app-cotact" class="d-flex align-items-center">
-                <div class="text-bg-light rounded-1 me-3 p-6 d-flex align-items-center justify-content-center">
-                  <img src="{{ URL::asset('build/images/svgs/icon-dd-mobile.svg') }}" alt="modernize-img" class="img-fluid"
-                    width="24" height="24" />
-                </div>
-                <div>
-                  <h6 class="mb-1 bg-hover-primary">Contact Application</h6>
-                  <span class="fs-2 d-block text-muted">2 Unsaved Contacts</span>
-                </div>
-              </a>
-            </li>
-            <li class="sidebar-item py-2">
-              <a href="/main/app-email" class="d-flex align-items-center">
-                <div class="text-bg-light rounded-1 me-3 p-6 d-flex align-items-center justify-content-center">
-                  <img src="{{ URL::asset('build/images/svgs/icon-dd-message-box.svg') }}" alt="modernize-img" class="img-fluid"
-                    width="24" height="24" />
-                </div>
-                <div>
-                  <h6 class="mb-1 bg-hover-primary">Email App</h6>
-                  <span class="fs-2 d-block text-muted">Get new emails</span>
-                </div>
-              </a>
-            </li>
-            <li class="sidebar-item py-2">
-              <a href="/main/page-user-profile" class="d-flex align-items-center">
-                <div class="text-bg-light rounded-1 me-3 p-6 d-flex align-items-center justify-content-center">
-                  <img src="{{ URL::asset('build/images/svgs/icon-dd-cart.svg') }}" alt="modernize-img" class="img-fluid"
-                    width="24" height="24" />
-                </div>
-                <div>
-                  <h6 class="mb-1 bg-hover-primary">User Profile</h6>
-                  <span class="fs-2 d-block text-muted">learn more information</span>
-                </div>
-              </a>
-            </li>
-            <li class="sidebar-item py-2">
-              <a href="/main/app-calendar" class="d-flex align-items-center">
-                <div class="text-bg-light rounded-1 me-3 p-6 d-flex align-items-center justify-content-center">
-                  <img src="{{ URL::asset('build/images/svgs/icon-dd-date.svg') }}" alt="modernize-img" class="img-fluid"
-                    width="24" height="24" />
-                </div>
-                <div>
-                  <h6 class="mb-1 bg-hover-primary">Calendar App</h6>
-                  <span class="fs-2 d-block text-muted">Get dates</span>
-                </div>
-              </a>
-            </li>
-            <li class="sidebar-item py-2">
-              <a href="/main/app-contact2" class="d-flex align-items-center">
-                <div class="text-bg-light rounded-1 me-3 p-6 d-flex align-items-center justify-content-center">
-                  <img src="{{ URL::asset('build/images/svgs/icon-dd-lifebuoy.svg') }}" alt="modernize-img" class="img-fluid"
-                    width="24" height="24" />
-                </div>
-                <div>
-                  <h6 class="mb-1 bg-hover-primary">Contact List Table</h6>
-                  <span class="fs-2 d-block text-muted">Add new contact</span>
-                </div>
-              </a>
-            </li>
-            <li class="sidebar-item py-2">
-              <a href="/main/app-notes" class="d-flex align-items-center">
-                <div class="text-bg-light rounded-1 me-3 p-6 d-flex align-items-center justify-content-center">
-                  <img src="{{ URL::asset('build/images/svgs/icon-dd-application.svg') }}" alt="modernize-img" class="img-fluid"
-                    width="24" height="24" />
-                </div>
-                <div>
-                  <h6 class="mb-1 bg-hover-primary">Notes Application</h6>
-                  <span class="fs-2 d-block text-muted">To-do and Daily tasks</span>
-                </div>
-              </a>
-            </li>
-            <ul class="px-8 mt-7 mb-4">
-              <li class="sidebar-item mb-3">
-                <h5 class="fs-5 fw-semibold">Quick Links</h5>
-              </li>
-              <li class="sidebar-item py-2">
-                <a class="fw-semibold text-dark" href="/main/page-pricing">Pricing Page</a>
-              </li>
-              <li class="sidebar-item py-2">
-                <a class="fw-semibold text-dark" href="/main/authentication-login">Authentication
-                  Design</a>
-              </li>
-              <li class="sidebar-item py-2">
-                <a class="fw-semibold text-dark" href="/main/authentication-register">Register Now</a>
-              </li>
-              <li class="sidebar-item py-2">
-                <a class="fw-semibold text-dark" href="/main/authentication-error">404 Error Page</a>
-              </li>
-              <li class="sidebar-item py-2">
-                <a class="fw-semibold text-dark" href="/main/app-notes">Notes App</a>
-              </li>
-              <li class="sidebar-item py-2">
-                <a class="fw-semibold text-dark" href="/main/page-user-profile">User Application</a>
-              </li>
-              <li class="sidebar-item py-2">
-                <a class="fw-semibold text-dark" href="/main/page-account-settings">Account Settings</a>
-              </li>
+    aria-labelledby="offcanvasWithBothOptionsLabel">
+    <nav class="sidebar-nav scroll-sidebar">
+        <div class="offcanvas-header justify-content-between">
+            <img src="{{  asset('img/logo.png') }}" alt="modernize-img" class="img-fluid" />
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body h-n80" data-simplebar="" data-simplebar>
+            <ul id="sidebarnav">
+                <li class="sidebar-item">
+                    <a class="sidebar-link {{ request()->is('dashboard') ? 'active' : '' }}"
+                        href="{{ route('dashboard') }}" aria-expanded="false">
+                        <span>
+                            <i class="ti ti-dashboard"></i>
+                        </span>
+                        <span class="hide-menu">Dashboard</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link has-arrow {{ request()->is('wedding-design1', 'wedding-design2', 'wedding-design3') ? 'in' : '' }}"
+                        href="javascript:void(0)" aria-expanded="false">
+                        <span class="d-flex">
+                            <i class="ti ti-chart-donut-3"></i>
+                        </span>
+                        <span class="hide-menu">Wedding Design</span>
+                    </a>
+                    <ul aria-expanded="false"
+                        class="collapse first-level {{ request()->is('wedding-design1', 'wedding-design2', 'wedding-design3') ? 'in' : '' }}">
+                        <li class="sidebar-item">
+                            <a class="sidebar-link {{ request()->is('wedding-design1') ? 'active' : '' }}"
+                                href="{{ route('wedding-design1.index') }}" aria-expanded="false">
+                                <div class="round-16 d-flex align-items-center justify-content-center">
+                                    <i class="ti ti-circle"></i>
+                                </div>
+                                <span class="hide-menu">Design 1</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="{{ route('wedding-design2.index') }}"
+                                class="sidebar-link {{ request()->is('wedding-design2') ? 'active' : '' }}">
+                                <div class="round-16 d-flex align-items-center justify-content-center">
+                                    <i class="ti ti-circle"></i>
+                                </div>
+                                <span class="hide-menu">Design 2</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="{{ route('wedding-design3.index') }}"
+                                class="sidebar-link {{ request()->is('wedding-design3') ? 'active' : '' }}">
+                                <div class="round-16 d-flex align-items-center justify-content-center">
+                                    <i class="ti ti-circle"></i>
+                                </div>
+                                <span class="hide-menu">Design 3</span>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                @if (auth()->user()->role == 1)
+                    <li class="sidebar-item">
+                        <a class="sidebar-link {{ request()->is('promo') ? 'active' : '' }}"
+                            href="{{ route('promo.index') }}" aria-expanded="false">
+                            <span>
+                                <i class="ti ti-basket"></i>
+                            </span>
+                            <span class="hide-menu">Promo</span>
+                        </a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a class="sidebar-link {{ request()->is('blog') ? 'active' : '' }}"
+                            href="{{ route('blog.index') }}" aria-expanded="false">
+                            <span>
+                                <i class="ti ti-notes"></i>
+                            </span>
+                            <span class="hide-menu">Blog</span>
+                        </a>
+                    </li>
+
+                    <li class="sidebar-item">
+                        <a class="sidebar-link has-arrow {{ request()->is('undangandigital', 'cetakfoto', 'seserahan', 'gambarin') ? 'in' : '' }}"
+                            href="javascript:void(0)" aria-expanded="false">
+                            <span class="d-flex">
+                                <i class="ti ti-chart-donut-3"></i>
+                            </span>
+                            <span class="hide-menu">Product</span>
+                        </a>
+                        <ul aria-expanded="false"
+                            class="collapse first-level {{ request()->is('undangandigital', 'cetakfoto', 'seserahan', 'gambarin') ? 'in' : '' }}">
+                            <li class="sidebar-item">
+                                <a class="sidebar-link {{ request()->is('undangandigital') ? 'active' : '' }}"
+                                    href="{{ route('undangandigital.index') }}" aria-expanded="false">
+                                    <div class="round-16 d-flex align-items-center justify-content-center">
+                                        <i class="ti ti-circle"></i>
+                                    </div>
+                                    <span class="hide-menu">Undangan Digital</span>
+                                </a>
+                            </li>
+                            <li class="sidebar-item">
+                                <a href="{{ route('cetakfoto.index') }}"
+                                    class="sidebar-link {{ request()->is('cetakfoto') ? 'active' : '' }}">
+                                    <div class="round-16 d-flex align-items-center justify-content-center">
+                                        <i class="ti ti-circle"></i>
+                                    </div>
+                                    <span class="hide-menu">Cetak Foto</span>
+                                </a>
+                            </li>
+                            <li class="sidebar-item">
+                                <a href="{{ route('seserahan.index') }}"
+                                    class="sidebar-link {{ request()->is('seserahan') ? 'active' : '' }}">
+                                    <div class="round-16 d-flex align-items-center justify-content-center">
+                                        <i class="ti ti-circle"></i>
+                                    </div>
+                                    <span class="hide-menu">Seserahan</span>
+                                </a>
+                            </li>
+
+                            <li class="sidebar-item">
+                                <a href="{{ route('gambarin.index') }}"
+                                    class="sidebar-link {{ request()->is('gambarin') ? 'active' : '' }}">
+                                    <div class="round-16 d-flex align-items-center justify-content-center">
+                                        <i class="ti ti-circle"></i>
+                                    </div>
+                                    <span class="hide-menu">Gambarin</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                @endif
             </ul>
-          </ul>
-        </li>
-        <li class="sidebar-item">
-          <a class="sidebar-link" href="/main/app-chat" aria-expanded="false">
-            <span>
-              <i class="ti ti-message-dots"></i>
-            </span>
-            <span class="hide-menu">Chat</span>
-          </a>
-        </li>
-        <li class="sidebar-item">
-          <a class="sidebar-link" href="/main/app-calendar" aria-expanded="false">
-            <span>
-              <i class="ti ti-calendar"></i>
-            </span>
-            <span class="hide-menu">Calendar</span>
-          </a>
-        </li>
-        <li class="sidebar-item">
-          <a class="sidebar-link" href="/main/app-email" aria-expanded="false">
-            <span>
-              <i class="ti ti-mail"></i>
-            </span>
-            <span class="hide-menu">Email</span>
-          </a>
-        </li>
-      </ul>
-    </div>
-  </nav>
+        </div>
+    </nav>
 </div>
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -6,55 +6,58 @@
 
 
 
-  <div class="d-block d-lg-none py-4">
-    <a href="main/index" class="text-nowrap logo-img">
-      <img src="{{ URL::asset('build/images/logos/dark-logo.svg') }}" class="dark-logo" alt="Logo-Dark" />
-      <img src="{{ URL::asset('build/images/logos/light-logo.svg') }}" class="light-logo" alt="Logo-light" />
-    </a>
-  </div>
-  <a class="navbar-toggler nav-icon-hover-bg rounded-circle p-0 mx-0 border-0" href="javascript:void(0)" data-bs-toggle="collapse"
-    data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <i class="ti ti-dots fs-7"></i>
-  </a>
-  <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-    <div class="d-flex align-items-center justify-content-between">
-      <a href="javascript:void(0)" class="nav-link nav-icon-hover-bg rounded-circle mx-0 ms-n1 d-flex d-lg-none align-items-center justify-content-center"
-        type="button" data-bs-toggle="offcanvas" data-bs-target="#mobilenavbar"
-        aria-controls="offcanvasWithBothOptions">
-        <i class="ti ti-align-justified fs-7"></i>
-      </a>
-      <ul class="navbar-nav flex-row ms-auto align-items-center justify-content-center">
-        <!-- ------------------------------- -->
-        <!-- start language Dropdown -->
-        <!-- ------------------------------- -->
-        <li class="nav-item nav-icon-hover-bg rounded-circle">
-          <a class="nav-link moon dark-layout" href="javascript:void(0)">
-            <i class="ti ti-moon moon"></i>
-          </a>
-          <a class="nav-link sun light-layout" href="javascript:void(0)">
-            <i class="ti ti-sun sun"></i>
-          </a>
-        </li>
-    
-        <li class="nav-item dropdown">
-          <a class="nav-link pe-0" href="javascript:void(0)" id="drop1" aria-expanded="false">
-            <div class="d-flex align-items-center">
-              <div class="user-profile-img">
-                <img src="{{ URL::asset('build/images/profile/user-1.jpg') }}" class="rounded-circle" width="35" height="35"
-                  alt="modernize-img" />
-              </div>
-            </div>
-          </a>
-          <div class="dropdown-menu content-dd dropdown-menu-end dropdown-menu-animate-up" aria-labelledby="drop1">
-            <x-headers.dd-profile/>
-          </div>
-        </li>
-        <!-- ------------------------------- -->
-        <!-- end profile Dropdown -->
-        <!-- ------------------------------- -->
-      </ul>
+    <div class="d-block d-lg-none py-4">
+        <a href="main/index" class="text-nowrap logo-img">
+            <img src="{{ asset('img/logo.png') }}" class="dark-logo" alt="Logo-Dark" />
+            <img src="{{ asset('img/logo.png') }}" class="light-logo" alt="Logo-light" />
+        </a>
     </div>
-  </div>
+    <a class="navbar-toggler nav-icon-hover-bg rounded-circle p-0 mx-0 border-0" href="javascript:void(0)"
+        data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+        aria-label="Toggle navigation">
+        <i class="ti ti-dots fs-7"></i>
+    </a>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <div class="d-flex align-items-center justify-content-between">
+            <a href="javascript:void(0)"
+                class="nav-link nav-icon-hover-bg rounded-circle mx-0 ms-n1 d-flex d-lg-none align-items-center justify-content-center"
+                type="button" data-bs-toggle="offcanvas" data-bs-target="#mobilenavbar"
+                aria-controls="offcanvasWithBothOptions">
+                <i class="ti ti-align-justified fs-7"></i>
+            </a>
+            <ul class="navbar-nav flex-row ms-auto align-items-center justify-content-center">
+                <!-- ------------------------------- -->
+                <!-- start language Dropdown -->
+                <!-- ------------------------------- -->
+                <li class="nav-item nav-icon-hover-bg rounded-circle">
+                    <a class="nav-link moon dark-layout" href="javascript:void(0)">
+                        <i class="ti ti-moon moon"></i>
+                    </a>
+                    <a class="nav-link sun light-layout" href="javascript:void(0)">
+                        <i class="ti ti-sun sun"></i>
+                    </a>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link pe-0" href="javascript:void(0)" id="drop1" aria-expanded="false">
+                        <div class="d-flex align-items-center">
+                            <div class="user-profile-img">
+                                <img src="{{ URL::asset('build/images/profile/user-1.jpg') }}" class="rounded-circle"
+                                    width="35" height="35" alt="modernize-img" />
+                            </div>
+                        </div>
+                    </a>
+                    <div class="dropdown-menu content-dd dropdown-menu-end dropdown-menu-animate-up"
+                        aria-labelledby="drop1">
+                        <x-headers.dd-profile />
+                    </div>
+                </li>
+                <!-- ------------------------------- -->
+                <!-- end profile Dropdown -->
+                <!-- ------------------------------- -->
+            </ul>
+        </div>
+    </div>
 </nav>
 <!-- ---------------------------------- -->
 <!-- End Vertical Layout Header -->
@@ -63,4 +66,4 @@
 <!-- ------------------------------- -->
 <!-- apps Dropdown in Small screen -->
 <!-- ------------------------------- -->
-<x-headers.dd-apps-mobile/>
+<x-headers.dd-apps-mobile />
