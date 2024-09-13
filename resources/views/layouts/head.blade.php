9f7f70676f9e86117231b0ff08ab7a2ba56113a8
Aldi Dharmawan <aldidharmawan700@gmail.com>
2024-09-13 14:37:24 +0700

    yes

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -6,8 +6,11 @@
 <!-- Favicon icon-->
 <link rel="shortcut icon" type="image/png" href="{{ asset('img/small-logo.jpg') }}" />
 <link rel="stylesheet" href="{{ asset('css/style-admin.css') }}">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+    integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <!-- Core Css -->
 <!-- <script src="{{ URL::asset('build/css/styles.css') }}"></script> -->
-@vite(['resources/scss/styles.scss'])
+<!-- In your Blade template -->
+@vite(['resources/scss/styles.scss', 'resources/js/app.js'])
