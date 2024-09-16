2d84293902ddfdcd24bb8513152a55fe784e0122
Aldi Dharmawan <aldidharmawan700@gmail.com>
2024-09-16 17:04:30 +0700

    yes

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,19 @@
+<!-- FOOTER -->
 <section class="footer" id="footer">
+    <div class="follow-us">
+        <span>Follow Us</span>
+        <a href="https://www.facebook.com/jejakkebahagiaan" target="_blank">
+            <img src="{{ asset('img/icon-facebook.svg') }}" alt="Facebook">
+        </a>
+        <a href="https://www.instagram.com/jejakkebahagiaan/" target="_blank">
+            <img src="{{ asset('img/icon-instagram.svg') }}" alt="Instagram">
+        </a>
+        <a href="https://www.tiktok.com/@jejakkebahagiaan?_t=8pjtPh8o2JL&_r=1" target="_blank">
+            <img src="{{ asset('img/icon-tiktok.svg') }}" alt="Tiktok">
+        </a>
+    </div>
     <span>
         © 2024 Jejak Kebahagiaan. All Rights Reserved.
     </span>
 </section>
+<!-- FOOTER END -->
