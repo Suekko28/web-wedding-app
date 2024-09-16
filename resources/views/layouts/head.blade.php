89699dbd26db5570beac160a365695e026f9400d
Aldi Dharmawan <aldidharmawan700@gmail.com>
2024-09-16 19:57:54 +0700

    yes

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -6,6 +6,7 @@
 <!-- Favicon icon-->
 <link rel="shortcut icon" type="image/png" href="{{ asset('img/small-logo.jpg') }}" />
 <link rel="stylesheet" href="{{ asset('css/style-admin.css') }}">
+<link rel="stylesheet" href="{{ asset('build/css/styles.css') }}">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
     integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
     crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -16,6 +17,6 @@
 
 
 <!-- Core Css -->
-<!-- <script src="{{ URL::asset('build/css/styles.css') }}"></script> -->
+<script src="{{ URL::asset('build/css/styles.css') }}"></script>
 <!-- In your Blade template -->
 @vite(['resources/scss/styles.scss'])
